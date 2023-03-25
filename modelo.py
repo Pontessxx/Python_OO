fac3e2f07096daeeb4d131c323a0c622ef53439d
Pontes <hpo.pontes@gmail.com>
2023-03-25 16:17:19 -0300

    def dar like

--- modelo.py
+++ modelo.py
@@ -1,20 +1,28 @@
 class Filme:#primeira classe
-    def __init__(self, nome, ano, duracao, like) -> None:
-        self.nome = nome
-        self.ano =ano
+    def __init__(self, nome, ano, duracao) -> None:
+        self.nome = nome.title()
+        self.ano = ano
         self.duaracao =duracao
-        self.like = like
+        self.like = 0
+
+    def dar_like(self):
+        self.like += 1
 
 class Serie: #manipulação de dados
-     def __init__(self, nome, ano, temporadas, like) -> None: 
-        self.nome = nome
-        self.ano =ano
+    def __init__(self, nome, ano, temporadas) -> None: 
+        self.nome = nome.title()
+        self.ano = ano
         self.temporadas =temporadas
-        self.like = like 
+        self.like = 0
+
+    def dar_like(self):
+        self.like += 1
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-print(f'Nome {vingadores.nome} - Ano: {vingadores.ano} -Temp: {vingadores.duaracao}')
+vingadores.dar_like()
+print(f'Nome {vingadores.nome} - Ano: {vingadores.ano} -Temp: {vingadores.duaracao} -Like:{vingadores.like}')
 
 atlanta = Serie('atlanta',2018, 2)
-print(f'Nome {atlanta.nome} - Ano: {atlanta.ano} -Temp: {atlanta.temporadas}')
+atlanta.dar_like()
+print(f'Nome {atlanta.nome} - Ano: {atlanta.ano} -Temp: {atlanta.temporadas} -Like: {atlanta.like}')
 
